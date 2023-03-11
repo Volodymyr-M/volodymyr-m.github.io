c184e9239962c02af54be11c09bbcc2380e79b03
Volodymyr Mazepa <v.mazepa@gmail.com>
2023-03-11 16:00:34 +0200

    javascript for collapsible nav

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -95,7 +95,29 @@ var lightbox = new SimpleLightbox('.simplelightbox-gallery a', {/* options */});
 
 
 
+// specific actions, mimicing bootstraps attributes
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
 
+window.addEventListener('click', (ev) => {
+  const elm = ev.target;
+  if (triggers.includes(elm)) {
+    const selector = elm.getAttribute('data-target');
+    collapse(selector, 'toggle');
+  }
+}, false);
+
+
+const fnmap = {
+  'toggle': 'toggle',
+  'show': 'add',
+  'hide': 'remove'
+};
+const collapse = (selector, cmd) => {
+  const targets = Array.from(document.querySelectorAll(selector));
+  targets.forEach(target => {
+    target.classList[fnmap[cmd]]('show');
+  });
+}
 
 
 
